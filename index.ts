bb24886fa597256f910c94f779944a1e7bf7628e
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2023-08-19 12:43:02 +0200

    Improved colors

--- index.ts
+++ index.ts
@@ -12,79 +12,71 @@ const blueBright = chalk.blueBright;
 const greenBright = chalk.greenBright;
 const redBright = chalk.redBright;
 
+const brown = chalk.hex('#A44A3F');
+const azure = chalk.hex('#7776BC');
+const lightBrown = chalk.hex('#D1B490');
+const lightAzure = chalk.hex('#30C5FF');
+const lightGreen = chalk.hex('#157145');
+
 log(greenBright(`Hello ${os.userInfo().username}\n\n`));
 
-log(grey(`Your system uptime is ${Math.floor(os.uptime() / 60)} mins\n\n`));
+log(lightBrown(`Your system uptime is ${Math.floor(os.uptime() / 60)} mins\n\n`));
 
 log(blueBright(`Stats of your system CPU\n`));
-log(blue(`CPU Architecture: ${os.arch()}`));
-log(blueBright(`CPU Model: ${os.cpus()[0].model}`));
+log(azure(`CPU Architecture: ${os.arch()}`));
+log(blueBright(`CPU Model: ${os.cpus()[0].model}\n`));
 
 await si.cpuTemperature().then((data) => {
 	os.cpus().forEach((cpu, index) => {
-		log(blue(`CPU n. ${index + 1}`));
-		log(green(`Speed: ${cpu.speed} MHz`));
+		log(azure(`CPU n. ${index + 1}`));
+		log(lightGreen(`Speed: ${cpu.speed} MHz`));
 		if (data.cores[index]) {
 			log(greenBright(`Temperature: ${data.cores[index]}°C`));
 		}
+		log('\n');
 	});
 });
 
-/*os.cpus().forEach((cpu, index) => {
-	log(blue(`CPU n. ${index + 1}`));
-	log(green(`Speed: ${cpu.speed} MHz`));
-	si.cpuTemperature().then((data) => {
-		log(greenBright(`Temperature: ${data.cores[index]}°C`));
-	});
-});*/
+log(blueBright(`RAM Stats\n`));
+log(azure(`Total: ${Math.floor(os.totalmem() / 1024 / 1024)} MB`));
+log(azure(`Currently in use: ${Math.floor((os.totalmem() - os.freemem()) / 1024 / 1024)} MB`));
 
-log(blueBright(`\nRAM Stats\n`));
-log(blue(`Total: ${Math.floor(os.totalmem() / 1024 / 1024)} MB`));
-log(blue(`Free: ${Math.floor(os.freemem() / 1024 / 1024)} MB`));
+log(blueBright('\n\nSystem Informations\n'));
 
-log(redBright('\nSystem Informations\n'));
-
-si.system().then((data) => {
-	log(red(`Manufacturer: ${data.manufacturer}`));
-	log(redBright(`Model: ${data.model}`));
+await si.system().then((data) => {
+	log(brown(`Manufacturer: ${data.manufacturer}`));
+	log(lightBrown(`Model: ${data.model}`));
 	if (data.virtual) {
-		log(redBright(`You are running on a virtual machine`));
-		log(red(data.virtualHost));
+		log(lightBrown(`You are running on a virtual machine`));
+		log(brown(data.virtualHost));
 	}
 
+	log('\n');
+});
+
+await si.baseboard().then((data) => {
+	log(brown(`Motherboard manufacturer: ${data.manufacturer}`));
+	log(lightBrown(`Model: ${data.model}`));
+	log(brown(`Version: ${data.version}`));
+
 	log('\n\n');
 });
 
-si.baseboard().then((data) => {
-	log(red(`Motherboard: ${data.manufacturer}`));
-	log(redBright(`Model: ${data.model}`));
-	log(red(`Version: ${data.version}`));
-
-	log('\n\n');
-});
-
-si.cpu().then((data) => {
-	log(red(`CPU: ${data.manufacturer}`));
-	log(redBright(`Brand: ${data.brand}`));
-	log(red(`Governor: ${data.governor}`));
-	log(redBright(`Cores: ${data.cores}`));
-	log(red(`Physical Cores: ${data.physicalCores}`));
-	log(redBright(`Socket type: ${data.socket}`));
+await si.cpu().then((data) => {
+	log(greenBright(`CPU Brand: ${data.brand}`));
+	log(lightGreen(`Governor: ${data.governor}`));
+	log(greenBright(`Cores: ${data.cores}`));
+	log(lightGreen(`Physical Cores: ${data.physicalCores}`));
+	log(lightGreen(`Socket type: ${data.socket}`));
 	if (data.virtualization) {
-		log(redBright(`Virtualization is supported on your system`));
+		log(greenBright(`Virtualization is supported on your system`));
 	}
 
 	log('\n\n');
 });
 
-si.osInfo().then((data) => {
-	if (data.platform === `linux`) {
-		log(red(`OS: ${data.distro}`));
-		log(redBright(`Kernel: ${data.kernel}`));
-		log(red(`${data.logofile}`));
-	} else {
-		log(red(`OS: ${data.distro}`));
-		log(redBright(`Kernel: ${data.kernel}`));
-		log(red(`${data.logofile}`));
-	}
+await si.osInfo().then((data) => {
+	log(lightAzure(`OS: ${data.distro}`));
+	log(azure(`Kernel: ${data.kernel}`));
+	log(lightAzure(`${data.logofile}`));
 });
