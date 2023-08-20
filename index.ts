6a95f947b81554da0143d1cfb83ae0601cfe0674
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2023-08-20 22:33:20 +0200

    Divided functions in choices

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import os from 'os';
 import chalk from 'chalk';
 import si from 'systeminformation';
 import figlet from 'figlet';
+import inquirer from 'inquirer';
 
 const log = console.log;
 
@@ -23,75 +24,112 @@ const lightGreen = chalk.hex('#157145');
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
+let initialChoice: string;
+
 function printName() {
-	const msg = `Hello ${os.userInfo().username}\n\n`;
+	log(grey(`Made with ❤️ by ${greenBright('beef_e')}\n`));
+
+	const msg = `Hello ${os.userInfo().username}\n`;
 	figlet(msg, { font: 'Colossal' }, (err, data) => {
 		log(greenBright(data));
 	});
 }
 
-function askChoice() {}
+async function askChoice() {
+	const question = await inquirer.prompt({
+		type: 'list',
+		name: 'choice',
+		message: 'What do you want to do?',
+		choices: [
+			{
+				name: 'Get system informations',
+				value: 'system',
+			},
+			{
+				name: 'Go to System monitoring',
+				value: 'monitoring',
+			},
+		],
+	});
 
+	initialChoice = question.choice;
+}
+
+async function getSysInfo() {
+	log(blueBright('\n\nSystem Informations\n'));
+
+	await si.system().then((data) => {
+		log(brown(`Manufacturer: ${data.manufacturer}`));
+		log(lightBrown(`Model: ${data.model}`));
+		if (data.virtual) {
+			log(lightBrown(`You are running on a virtual machine`));
+			log(brown(data.virtualHost));
+		}
+
+		log('\n');
+	});
+
+	await si.baseboard().then((data) => {
+		log(brown(`Motherboard manufacturer: ${data.manufacturer}`));
+		log(lightBrown(`Model: ${data.model}`));
+		log(brown(`Version: ${data.version}`));
+
+		log('\n\n');
+	});
+
+	await si.cpu().then((data) => {
+		log(greenBright(`CPU Brand: ${data.brand}`));
+		log(lightGreen(`Governor: ${data.governor}`));
+		log(greenBright(`Cores: ${data.cores}`));
+		log(lightGreen(`Physical Cores: ${data.physicalCores}`));
+		log(lightGreen(`Socket type: ${data.socket}`));
+		if (data.virtualization) {
+			log(greenBright(`Virtualization is supported on your system`));
+		}
+
+		log('\n\n');
+	});
+
+	await si.osInfo().then((data) => {
+		log(lightAzure(`OS: ${data.distro}`));
+		log(azure(`Kernel Version: ${data.kernel}`));
+		log(lightAzure(`${data.logofile}`));
+	});
+}
+
+async function monitorSys() {
+	log(lightBrown(`Your system uptime is ${Math.floor(os.uptime() / 60)} mins\n\n`));
+
+	log(blueBright(`Stats of your system CPU\n`));
+	log(azure(`CPU Architecture: ${os.arch()}`));
+	log(blueBright(`CPU Model: ${os.cpus()[0].model}\n`));
+
+	await si.cpuTemperature().then((data) => {
+		os.cpus().forEach((cpu, index) => {
+			log(azure(`CPU n. ${index + 1}`));
+			log(lightGreen(`Speed: ${cpu.speed} MHz`));
+			if (data.cores[index]) {
+				log(greenBright(`Temperature: ${data.cores[index]}°C`));
+			}
+			log('\n');
+		});
+	});
+
+	log(blueBright(`RAM Stats\n`));
+	log(azure(`Total: ${Math.floor(os.totalmem() / 1024 / 1024)} MB`));
+	log(azure(`Currently in use: ${Math.floor((os.totalmem() - os.freemem()) / 1024 / 1024)} MB`));
+}
+
+//! Inizio Programma
 printName();
 await wait(350);
 
-log(lightBrown(`Your system uptime is ${Math.floor(os.uptime() / 60)} mins\n\n`));
-
-log(blueBright(`Stats of your system CPU\n`));
-log(azure(`CPU Architecture: ${os.arch()}`));
-log(blueBright(`CPU Model: ${os.cpus()[0].model}\n`));
-
-await si.cpuTemperature().then((data) => {
-	os.cpus().forEach((cpu, index) => {
-		log(azure(`CPU n. ${index + 1}`));
-		log(lightGreen(`Speed: ${cpu.speed} MHz`));
-		if (data.cores[index]) {
-			log(greenBright(`Temperature: ${data.cores[index]}°C`));
-		}
-		log('\n');
-	});
-});
-
-log(blueBright(`RAM Stats\n`));
-log(azure(`Total: ${Math.floor(os.totalmem() / 1024 / 1024)} MB`));
-log(azure(`Currently in use: ${Math.floor((os.totalmem() - os.freemem()) / 1024 / 1024)} MB`));
-
-log(blueBright('\n\nSystem Informations\n'));
-
-await si.system().then((data) => {
-	log(brown(`Manufacturer: ${data.manufacturer}`));
-	log(lightBrown(`Model: ${data.model}`));
-	if (data.virtual) {
-		log(lightBrown(`You are running on a virtual machine`));
-		log(brown(data.virtualHost));
+await askChoice().then(() => {
+	if (initialChoice === 'system') {
+		// System informations
+		getSysInfo();
+	} else if (initialChoice === 'monitoring') {
+		// System monitoring
+		monitorSys();
 	}
-
-	log('\n');
-});
-
-await si.baseboard().then((data) => {
-	log(brown(`Motherboard manufacturer: ${data.manufacturer}`));
-	log(lightBrown(`Model: ${data.model}`));
-	log(brown(`Version: ${data.version}`));
-
-	log('\n\n');
-});
-
-await si.cpu().then((data) => {
-	log(greenBright(`CPU Brand: ${data.brand}`));
-	log(lightGreen(`Governor: ${data.governor}`));
-	log(greenBright(`Cores: ${data.cores}`));
-	log(lightGreen(`Physical Cores: ${data.physicalCores}`));
-	log(lightGreen(`Socket type: ${data.socket}`));
-	if (data.virtualization) {
-		log(greenBright(`Virtualization is supported on your system`));
-	}
-
-	log('\n\n');
-});
-
-await si.osInfo().then((data) => {
-	log(lightAzure(`OS: ${data.distro}`));
-	log(azure(`Kernel Version: ${data.kernel}`));
-	log(lightAzure(`${data.logofile}`));
 });
